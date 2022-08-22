f3f426ab69cd4ce06ae7eb8d54e3734ac8adda14
DgokeRFeed <popovvova99@yandex.ru>
2022-08-23 02:03:29 +0300

    add user edit page, add email validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,48 @@
 class UsersController < ApplicationController
   def new
+    session[:current_time] = Time.now
     @user = User.new
   end
 
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
+    @user = User.new(user_params)
 
-    user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Вы успешно зарегистрировались"
+    else
+      flash.now[:alert] = "Неверно введены данные"
+      render :new
+    end
+  end
 
-    session[:user_id] = user.id
+  def edit
+    @user = User.find(params[:id])
+  end
 
-    redirect_to root_path, notice: "Вы успешно зарегистрировались"
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to root_path, notice: "Данные пользователя обновлены"
+    else
+      flash.now[:alert] = "Сохранить данные не удалось!"
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    session.delete(:user_id)
+
+    redirect_to new_session_path, notice: "Страница пользователя удалена"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :nickname, :email, :password, :password_confirmation)
   end
 end

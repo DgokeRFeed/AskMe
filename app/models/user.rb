6028fee532901a57e03b6584c0665db43aa382fd
DgokeRFeed <popovvova99@yandex.ru>
2022-08-23 23:35:53 +0300

    correct regex for nickname validation, add uniq index in DB for nickname and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  before_save :downcase_nickname
+  before_validation :downcase_nickname, :downcase_email
 
   validates :email,
             presence: true,
@@ -11,10 +11,16 @@ class User < ApplicationRecord
   validates :nickname,
             presence: true ,
             uniqueness: true,
-            format: { with: /\A[a-zA-Z\d].\w*\z/ },
+            format: { with: /\A[a-zA-Zа-яА-Я\d]([a-zA-Zа-яА-Я\d_]*[a-zA-Zа-яА-Я\d])?\Z/ },
             length: { maximum: 40 }
+
+  private
 
   def downcase_nickname
     nickname.downcase!
+  end
+
+  def downcase_email
+    email.downcase!
   end
 end
